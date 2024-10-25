c3c71a95fe17acbf034bcb4ca610f483e17fe117
Julian Baumann <julian.baumann@cyancor.com>
2024-10-25 22:39:12 +0200

    Fixed native lib packaging in c#, added c# github action

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,4 @@
-rustup target add \
-    x86_64-pc-windows-msvc \
-    aarch64-pc-windows-msvc
+rustup target add x86_64-pc-windows-msvc aarch64-pc-windows-msvc
 
 cargo install uniffi-bindgen-cs --git https://github.com/NordSecurity/uniffi-bindgen-cs --tag v0.8.3+v0.25.0
 
